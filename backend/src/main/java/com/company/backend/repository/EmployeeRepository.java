1282e483f52906df1246f019aa23250017000fb2
root <root@training.vzopgj35mulubau1mzsmz1exog.dx.internal.cloudapp.net>
2025-12-06 09:39:09 +0000

    used jdk 17 for code compatibility

--- backend/src/main/java/com/company/backend/repository/EmployeeRepository.java
+++ backend/src/main/java/com/company/backend/repository/EmployeeRepository.java
@@ -3,4 +3,6 @@ package com.company.backend.repository;
 import com.company.backend.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> { }
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
+
